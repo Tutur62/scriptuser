5e1c719576e0e4e0c9cb2140ada26ea62d403409
Arthur Lemaire <31710356+Tutur62@users.noreply.github.com>
2018-02-22 21:37:34 +0100

    Initial commit

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+lst1=$(cat /home/iutcb/Bureau/users.txt)
+for i in $lst1
+do
+a1=$(echo $i | awk -F: '{print $1}')
+useradd $a1
+a2=$(echo $i | awk -F: '{print $2}')
+echo -e "$a2\n$a2" | passwd $a1
+a3=$(echo $i | awk -F: '{print $3}')
+addgroup $a1 $a3
+echo "L'utilisateur "$a1" faisant parti du groupe "$a3" a comme mot de passe "$a2
+done
